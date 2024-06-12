9ff5dcec014b5b39c9d4695d719959edcd0ebf05
Ahmed Jarif <ahmedjarif1230@gmail.com>
2024-06-12 09:52:05 +0600

    Update niji.js

--- scripts/cmds/niji.js
+++ scripts/cmds/niji.js
@@ -1,74 +1,45 @@
 const axios = require("axios");
+
 module.exports = {
-    config: {
-        name: "niji",
-        aliases: ["nijijourney", "art"],
-        version: "1.0",
-        author: "rehat--",
-        countDown: 0,
-        role: 0,
-        description: "Text to Image",
-        category: "𝗔𝗜-𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗",
-        guide: {
-    en: `{pn} <prompt> --ar [ratio], [preset], [style], or reply to an image\n\n Example: {pn} 1girl, cute face, masterpiece, best quality --ar 16:9 --preset 2 --style 6\n\nAvailable Styles:\n1. Cinematic\n2. Photographic\n3. Anime\n4. Manga\n5. Digital Art\n6. Pixel Art\n7. Fantasy Art\n8. Neon Punk\n9. 3D Model\n\nAvailable Preset Styles:\n1. Standard v3.0\n2. Standard v3.1\n3. Light v3.1\n4. Heavy v3.1\nThanks for using the project`,
-      }
+  config: {
+    name: "niji",
+    version: "3.8",
+    author: "ArYAN",
+    countDown: 10,
+    category: "media",
+    longDescription: {
+      en: 'Generate Images using NIJI API',
     },
-
-    onStart: async function({ api, args, message, event }) {
-        try {
-            let prompt = "";
-            let style = "";
-            let imageUrl = "";
-            let preset = "";
-            let aspectRatio = ""; 
-
-            const styleIndex = args.indexOf("--style");
-            if (styleIndex !== -1 && args.length > styleIndex + 1) {
-                style = args[styleIndex + 1];
-                args.splice(styleIndex, 2); 
-            }
-
-            const presetIndex = args.indexOf("--preset");
-            if (presetIndex !== -1 && args.length > presetIndex + 1) {
-                preset = args[presetIndex + 1];
-                args.splice(presetIndex, 2); 
-            }
-            
-            const aspectIndex = args.indexOf("--ar");
-            if (aspectIndex !== -1 && args.length > aspectIndex + 1) {
-                aspectRatio = args[aspectIndex + 1];
-                args.splice(aspectIndex, 2); 
-            }
-
-            if (event.type === "message_reply" && event.messageReply.attachments && event.messageReply.attachments.length > 0 && ["photo", "sticker"].includes(event.messageReply.attachments[0].type)) {
-                imageUrl = encodeURIComponent(event.messageReply.attachments[0].url);
-            } else if (args.length === 0) {
-                message.reply("𝙿𝚕𝚎𝚊𝚜𝚎 𝚙𝚛𝚘𝚟𝚒𝚍𝚎 𝚊 𝚙𝚛𝚘𝚖𝚙𝚝 𝚘𝚛 𝚛𝚎𝚙𝚕𝚢 𝚝𝚘 𝚊𝚗 𝚒𝚖𝚊𝚐𝚎.");
-                return;
-            }
-            
-            if (args.length > 0) {
-                prompt = args.join(" ");
-            }
-
-            
-            let apiUrl = `https://rehatdesu.xyz/api/imagine/niji?prompt=${encodeURIComponent(prompt)}.&aspectRatio=${aspectRatio}&apikey=rehat86&style=${style}&preset=${preset}`;
-            if (imageUrl) {
-                apiUrl += `&imageUrl=${imageUrl}`;
-            }
-
-            const processingMessage = await message.reply(" 𓃝 Initiating request");
-            const response = await axios.post(apiUrl);
-            const img = response.data.url;
-
-            await message.reply({
-                body: `✨ | 𝙷𝚎𝚛𝚎'𝚜 𝚈𝚘𝚞𝚛 𝙶𝚎𝚗𝚎𝚛𝚊𝚝𝚎𝚍 𝙸𝚖𝚊𝚐𝚎 \n\n📥 | 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙻𝚒𝚗𝚔:\n${img}`,
-                attachment: await global.utils.getStreamFromURL(img)
-            });
-
-        } catch (error) {
-            console.error(error);
-            message.reply("𝙰𝚗 𝚎𝚛𝚛𝚘𝚛 𝚘𝚌𝚌𝚞𝚛𝚛𝚎𝚍.");
-        }
+    guide: {
+      en: '${p}niji [ prompt ]'
     }
-}; 
+  },
+  onStart: async function ({ message, api, args, event }) {
+    const text = args.join(' ');
+    if (!text) {
+      return message.reply("⛔|𝗜𝗻𝘃𝗮𝗹𝗶𝗱 \n━━━━━━━━━━━━\n\n➤ Please provide a prompt with model");
+    }
+    
+    const baseURL = `https://itsaryan.onrender.com/api/niji?prompt=${text}`;
+ 
+   message.reply("Generating your image, please wait a few moments.", async (err, info) => {
+      
+   const startTime = new Date().getTime(); // Define startTime
+    
+
+    api.setMessageReaction("⏳", event.messageID, () => {}, true);
+   
+    const endTime = new Date().getTime(); // Move endTime inside the asynchronous block
+      const timeTaken = (endTime - startTime) / 1000; 
+      
+    message.reply({ 
+        body: `🖼️ [ 𝗡𝗜𝗝𝗜 ]\n\nHere is your generated image\nTime taken: ${timeTaken} seconds`,
+        attachment: await global.utils.getStreamFromURL(baseURL)
+      });
+
+      let ui = info.messageID;
+      message.unsend(ui);
+      api.setMessageReaction("✅", event.messageID, () => {}, true);
+    });
+  }
+};
